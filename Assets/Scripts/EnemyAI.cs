311bc87856a60817b11ab5cf0c1d1562128915bc
Cody Caro <codyccaro@gmail.com>
2018-07-09 13:14:04 -0700

    Got basic enemy ai in.

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+    [SerializeField]
+    float m_speed;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        //move enemy down
+        transform.Translate(Vector3.down * m_speed * Time.deltaTime);
+
+        //when off the screen respawn back at the top in a random x position
+        if(transform.position.y < -6.17f)
+        {
+            float randomXPos = Random.Range(-8f,8f);
+            transform.position = new Vector3(randomXPos, 6.17f, 0f);
+        }
+	}
+}
